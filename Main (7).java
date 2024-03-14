246f3bc7a23035d215a3f0ec9bdc8743c30764d8
Cortinovis <160122402+shortinovis@users.noreply.github.com>
2024-03-14 11:57:58 +0100

    Add files via upload

---
+++ Main (7).java
@@ -0,0 +1,80 @@
+import java.util.SCanner;
+
+public class Main {
+	public static void main(String[] args) {
+	    Scanner t = new Scanner(System.in);
+	    String [] parole
+	do{
+	    int opzione;
+		System.out.println("scegli  opzione:");
+		System.out.println("1-Aggiunta di un nome;");
+		System.out.println("2-Cancellazione del singolo nome;");
+		System.out.println("3-Ricerca sequenziale di un nome;");
+		System.out.println("4-Visualizza nomi ripetuti con numero ripetizioni";);
+		System.out.println("5-Modifica di un nome;");
+		System.out.println("6-Visualizzazione di tutti i nomi presenti;");
+		System.out.println("7-Ricerca del nome più lungo e più corto;");
+		System.out.println("8-Cancellazione di tutte le occorrenze di un nome;");
+		System.out.println("0-esci");
+		opzione=t.nextInt();
+		
+		switch (opzione){
+		    case 1:
+		        
+		        
+		        break;
+		    case 2:
+		        
+		        
+		        break;
+		    case 3:
+		        
+		        
+		        break;
+		    case 4:
+		        
+		        
+		        break;
+		    case 5:
+		        
+		        
+		        break;
+		    case 6:
+		        
+		        
+		        break;
+		    case 7:
+		        
+		        
+		        break;
+		    case 8:
+		        
+		        
+		        break;
+		    case 0:
+		        
+		        
+		        System.out.println("grazie per aver usate il servizio cortinovis, alla prossima!")
+		        break;
+		    
+		}
+	}while(opzione !=0);
+}
+private static String void IsAggiunta (int[] nome, parola, conta) {
+    nome[conta-1]=parola;
+    return nome;
+}
+Private static void IsCancellazione(){
+}
+Private static void IsRicerca(){
+}
+Private static void IsVisualizzaTutto(){
+}
+Private static void IsModifica(){
+}
+Private static void IsVisualizzaSingolo(){
+}
+Private static void IsRicercaLC(){
+}
+Private static void IsCancellazione(){
+} 
